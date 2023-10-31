31ab45f48a800b5ceeee8d715bd9b6b58dee5aa3
kundnanl <kundnanl@sheridancollege.ca>
2023-10-31 11:25:36 -0400

    INITIAL SERVER SETUP

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,28 +1,27 @@
-import { InitialModal } from "@/components/modals/initial-modal";
-import { db } from "@/lib/db";
-import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
 
-const SetUpPage = async () => {
-    const profile =  await initialProfile();
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/modals/initial-modal";
 
-    const server = await db.server.findFirst({
-        where: {
-            member: {
-                some: {
-                    profileId: profile.id
-                }
-            }
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
         }
-    });
-
-    if (server) {
-        return redirect(`/server/${server.id}`);
+      }
     }
+  });
 
-    return ( 
-        <InitialModal />
-     );
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />;
 }
  
-export default SetUpPage;
+export default SetupPage;
